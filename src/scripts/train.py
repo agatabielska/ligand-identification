e0ed528ac7e7086a52dcbd17f3be5f85e13b896b
Adam Mazur <adammazur445@gmail.com>
2025-11-23 22:17:53 +0100

    feat: Add hydra configs

--- src/scripts/train.py
+++ src/scripts/train.py
@@ -3,8 +3,11 @@ from src.pipeline.dataset import BlobDataset
 from pytorch_lightning.callbacks import ModelCheckpoint
 from torch.utils.data import DataLoader
 import pytorch_lightning as pl
+from omegaconf import DictConfig
 import numpy as np
+import hydra
 import torch
+import sys
 
 
 def transform(npz):
@@ -20,44 +23,67 @@ def transform(npz):
     return torch.from_numpy(points)
 
 
-train_dataset = BlobDataset(
-    path="data/processed/train", transform=transform, cache=True
-)
-val_dataset = BlobDataset(path="data/processed/test", transform=transform, cache=True)
+@hydra.main(config_path="../../configs", config_name="config", version_base=None)
+def main(cfg: DictConfig):
+    print(cfg)
+    sys.exit(0)
 
-train_dataloader = DataLoader(
-    train_dataset, batch_size=128, shuffle=True, num_workers=64, pin_memory=True
-)
-val_dataloader = DataLoader(
-    val_dataset, batch_size=128, shuffle=False, num_workers=64, pin_memory=True
-)
+    train_dataset = BlobDataset(
+        path="data/processed/train",
+        transform=transform,
+        cache=cfg.machine.cache_dataset,
+    )
 
+    val_dataset = BlobDataset(
+        path="data/processed/test", transform=transform, cache=cfg.machine.cache_dataset
+    )
 
-checkpoint_callback = ModelCheckpoint(
-    monitor="val_loss",
-    dirpath="checkpoints/",
-    filename="model-{epoch:02d}-{val_loss:.2f}",
-    mode="min",
-)
+    train_dataloader = DataLoader(
+        train_dataset,
+        batch_size=cfg.machine.batch_size,
+        shuffle=True,
+        num_workers=cfg.machine.num_workers,
+        pin_memory=cfg.machine.pin_memory,
+    )
 
-trainer = pl.Trainer(
-    max_epochs=10,
-    callbacks=[checkpoint_callback],
-    accelerator="auto",
-    devices=1,
-    log_every_n_steps=10,
-)
+    val_dataloader = DataLoader(
+        val_dataset,
+        batch_size=cfg.machine.batch_size,
+        shuffle=False,
+        num_workers=cfg.machine.num_workers,
+        pin_memory=cfg.machine.pin_memory,
+    )
 
-model = CliffordSteerableNetwork(
-    p=3,
-    q=0,
-    in_channels=3,
-    hidden_channels=[32, 64, 128],
-    out_channels=219,
-    n_shells=3,
-    kernel_size=3,
-    learning_rate=1e-3,
-)
+    checkpoint_callback = ModelCheckpoint(
+        monitor="val_loss",
+        dirpath="checkpoints/",
+        filename="model-{epoch:02d}-{val_loss:.2f}",
+        mode="min",
+    )
 
+    trainer = pl.Trainer(
+        max_epochs=cfg.train.epochs,
+        callbacks=[checkpoint_callback],
+        accelerator="auto",
+        devices=cfg.machine.devices,
+        log_every_n_steps=10,
+    )
 
-trainer.fit(model, train_dataloader, val_dataloader)
+    if cfg.model.type == "clifford":
+        model = CliffordSteerableNetwork(
+            p=cfg.model.p,
+            q=cfg.model.q,
+            in_channels=cfg.model.in_channels,
+            hidden_channels=cfg.model.hidden_channels,
+            out_channels=cfg.train.out_channels,
+            n_shells=cfg.model.n_shells,
+            kernel_size=cfg.model.kernel_size,
+            learning_rate=cfg.train.learning_rate,
+        )
+    else:
+        raise ValueError(f"Unknown model type: {cfg.model.type}")
+
+    trainer.fit(model, train_dataloader, val_dataloader)
+
+if __name__ == "__main__":
+    main()
